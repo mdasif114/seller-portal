40b135d306c26718652f4fcefd542be011085dff
Mohammed Asif <mohammed.asif@ericsson.com>
2020-08-20 18:10:54 +0800

    Update: First execution success

---
+++ src/main/java/com/seller/portal/controller/UserRegistrationController.java
@@ -0,0 +1,50 @@
+package com.seller.portal.controller;
+
+import com.seller.portal.entity.User;
+import com.seller.portal.service.UserService;
+import com.seller.portal.validators.UserRegistrationDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
+
+@Controller
+@RequestMapping("/user_registration")
+public class UserRegistrationController {
+
+    @Autowired
+    private UserService userService;
+
+    @ModelAttribute("user")
+    public UserRegistrationDto userRegistrationDto() {
+        return new UserRegistrationDto();
+    }
+
+    @GetMapping
+    public String showRegistrationForm(Model model) {
+        return "user_registration";
+    }
+
+    @PostMapping
+    public String registerUserAccount(@ModelAttribute("user") @Valid UserRegistrationDto userDto,
+                                      BindingResult result) {
+
+        User existing = userService.findByEmail(userDto.getEmail());
+        if (existing != null) {
+            result.rejectValue("email", null, "There is already an account registered with that email");
+        }
+
+        if (result.hasErrors()) {
+            return "user_registration";
+        }
+
+        userService.save(userDto);
+        return "redirect:/user_registration?success";
+    }
+}
